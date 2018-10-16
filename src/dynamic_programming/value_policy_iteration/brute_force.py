9891afe6699b08a87f577d724e530319461a2c9c
johannesharmse <johannesharmse91@gmail.com>
2018-10-15 21:13:31 -0700

    Add to brute force

--- src/dynamic_programming/value_policy_iteration/brute_force.py
+++ src/dynamic_programming/value_policy_iteration/brute_force.py
@@ -12,23 +12,65 @@ def execute(env, policy, episodeLength=100, render=False):
         render: boolean to turn rendering on/off
     """
 
-    # intit reward and start env
+    # intit reward and start 
+    # observation (assuming this is slightly random)
     totalReward = 0
     start = env.reset()
 
     # iterate through episode
     for t in range(episodeLength):
+        
         if render:
             env.render()
 
+        # get action for current state
+        action = policy[start]
+        # perform state action and see what happens
+        start, reward, done, _ = env.step(action)
+        # collect reward
+        totalReward += reward
+
+        if done:
+            break
+
     return totalReward
+
+
+# Evaluation
+def evaluatePolicy(env, policy, n_episodes=100):
+    """Calculate mean score of policy"""
+    # init
+    totalReward = 0.0
+
+    # run for a number of episodes
+    for _ in range(n_episodes):
+        # add 
+        totalReward += execute(env, policy)
+    
+    # average reward of input policy
+    return totalReward / n_episodes
 
 
 def gen_random_policy():
     """Generate random policy"""
     return np.random.choice(4, size=16)
 
-if __name__ == "main":
+if __name__ == "__main__":
     env = gym.make('FrozenLake-v0')
-    policy = gen_random_policy()
-    execute(env, policy)
+    # number of random policies to consider
+    n_policies = 1000
+    
+    # time complexity
+    startTime = time.time()
+    
+    # generate random policies
+    policy_set = [gen_random_policy() for _ in range(n_policies)]
+
+    # score each random policy
+    policy_score = [evaluatePolicy(env, p) for p in policy_set]
+
+    # time complexity
+    endTime = time.time()
+
+    print("Best policy score = %.2f." % np.max(policy_score))
+    print("Time Duration: %.2f seconds" % (endTime - startTime))
--- src/dynamic_programming/value_policy_iteration/scratch.py
+++ src/dynamic_programming/value_policy_iteration/scratch.py
@@ -1,3 +1,5 @@
 import numpy as np
 
-np.random.choice(4, size=(16))
+print(np.random.choice(4, size=16))
+
+
