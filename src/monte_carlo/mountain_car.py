e9d4a966d1c5ba3b830dda99525088603beed34d
johannesharmse <johannesharmse91@gmail.com>
2018-10-26 00:18:55 -0700

    Bug fixing

--- src/monte_carlo/mountain_car.py
+++ src/monte_carlo/mountain_car.py
@@ -1,7 +1,9 @@
 from mc import FiniteMCModel as MC
 import gym
 import numpy as np
+import random
 
+# print(np.array([np.random.uniform(-0.6, -0.4), 0]))
 
 # use mountaincar env
 env = gym.make('MountainCar-v0')
@@ -18,7 +20,7 @@ max_speed = 0.07
 # spatial position and possilbe speed
 S = [(x, y, z) for x in np.linspace(min_position, max_position, 10) 
                 for y in np.linspace(min_position, max_position, 10) 
-                for z in np.linspace(0., 0.07, 10)]
+                for z in np.linspace(0., max_speed, 10)]
 
 # three actions
 # left, nothing, right
@@ -29,13 +31,23 @@ m = MC(S, A, epsilon=1)
 
 # loop through episodes
 for i in range(1, episodes+1):
+    print(i)
     ep = []
     # reset position of cart for episode
     # https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
     # shows random uniform reset
-    observation = env.reset()
+    # observation = env.reset()
+    observation = random.choice(S)
 
-    
+    while True:
+        # choose policy
+        # based on random action selected by b methid 
+        action = m.choose_action(m.b, observation)
+
+        # simulate
+
+
+
 
 
     
