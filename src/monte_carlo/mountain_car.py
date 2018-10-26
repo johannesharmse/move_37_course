39ccefe0b76d388464b65ff5c807436fc783eb4c
johannesharmse <johannesharmse91@gmail.com>
2018-10-26 00:41:36 -0700

    Chamge everyting...

--- src/monte_carlo/mountain_car.py
+++ src/monte_carlo/mountain_car.py
@@ -6,38 +6,29 @@ import random
 # print(np.array([np.random.uniform(-0.6, -0.4), 0]))
 
 # use mountaincar env
-env = gym.make('MountainCar-v0')
+env = gym.make('Taxi-v2')
 
-episodes = int(10e4)
-
-# 2d space
-# velocity within a state
-min_position = -1.2
-max_position = 0.6
-max_speed = 0.07
+episodes = int(10e6)
 
 # states
-# spatial position and possilbe speed
-S = [(x, y, z) for x in np.linspace(min_position, max_position, 10) 
-                for y in np.linspace(min_position, max_position, 10) 
-                for z in np.linspace(0., max_speed, 10)]
+# S = [(row, col, passidx, destidx) for row in range(5) 
+#                                     for col in range(5)
+#                                     for passidx in range(5) 
+#                                     for destidx in range(4)]
+S = 500
 
 # three actions
 # left, nothing, right
-A = 2
+A = 5
 
 # define MC class
 m = MC(S, A, epsilon=1)
 
 # loop through episodes
 for i in range(1, episodes+1):
-    print(i)
     ep = []
-    # reset position of cart for episode
-    # https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
-    # shows random uniform reset
-    # observation = env.reset()
-    observation = random.choice(S)
+    # reset state
+    observation = env.reset()
 
     while True:
         # choose policy
@@ -45,6 +36,16 @@ for i in range(1, episodes+1):
         action = m.choose_action(m.b, observation)
 
         # simulate
+        next_observation, reward, done, _ = env.step(action)
+        ep.append((observation, action, reward))
+        observation = next_observation
+        if done:
+            break
+
+    m.update_Q(ep)
+    m.epsilon = max((episodes-i)/eps, 0.1)
+
+    print("Final expected returns: {}".format(m.score(env, m.pi, n_samples=10000)))
 
 
 
