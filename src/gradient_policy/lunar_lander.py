cfaca2a6949f1e6661f0791fea09bab5539043a2
johannesharmse <johannesharmse91@gmail.com>
2018-11-13 13:49:16 -0800

    Add rewards function

--- src/gradient_policy/lunar_lander.py
+++ src/gradient_policy/lunar_lander.py
@@ -4,6 +4,21 @@ import tensorflow as tf
 import numpy as np
 import gym
 
+def discount_and_normalize_rewards(episode_rewards):
+    """Calculate normalized episode reward"""
+
+    discounted_episode_rewards = np.zeros_like(episode_rewards)
+    cumulative = 0.0
+    for i in reversed(range(len(episode_rewards))):
+        cumulative = cumulative * gamma + episode_rewards[i]
+        discounted_episode_rewards[i] = cumulative
+    
+    mean = np.mean(discounted_episode_rewards)
+    std = np.std(discounted_episode_rewards)
+    discounted_episode_rewards = (discounted_episode_rewards - mean) / (std)
+    
+    return discounted_episode_rewards
+
 if __name__ == "__main__":
     env = gym.make('LunarLander-v2')
 
@@ -18,6 +33,7 @@ if __name__ == "__main__":
     # discount rate
     gamma = 0.95
 
-    
+
+
 
     # sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
